977ddc5079f4bd87922bfcf3e01b4ff5839d48a9
JN513 <jn5.guape@gmail.com>
2023-11-17 21:07:01 -0300

    atualizando dependencias e atualizando cliente

---
+++ remote_flash.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import os
+import sys
+import requests
+import argparse
+
+current_directory = os.getcwd()
+
+
+parser = argparse.ArgumentParser(
+    prog="color_image.py",
+    description="Transforme imagens coloridas em escala de cinza e preto e branco",
+    epilog="Text at the bottom of help",
+)
+
+parser.add_argument("filename", type=str, help="nome do arquivo a ser gravado")
+parser.add_argument(
+    "-f",
+    "--full_path",
+    action="store_true",
+    default=False,
+    help="O nome do arquivo passado e o path completo ou não",
+)
+
+parser.add_argument(
+    "-b",
+    "--board",
+    type=str,
+    default="tangnano20k",
+    help="Placa a ser gravada",
+)
+
+args = parser.parse_args()
+
+file_path = ""
+
+if not args.full_path:
+    file_path = current_directory + "/" + args.filename
+else:
+    file_path = args.filename
+
+file = open(file_path, "r")
+
+if not file:
+    file.close()
+
+    sys.exit()
+
+url = "http://10.68.10.71:8000/flash_"
+board = ""
+
+if args.board == "tangnano20k":
+    board = "20k"
+else:
+    board = "9k"
+
+print("Log: iniciado request ao servidor.")
+
+r = requests.post(f"{url}{board}",
+                  files={"file": file})
+
+print("Log: request concluida.")
+
+request_body = r.json()
+
+if r.status_code != 201:
+    if "error" in request_body:
+        print(f"Log: message de saida: {request_body['error']}")
+    else:
+        print(f"Log: arquivo gerado: {request_body['filename']}")
+        print(f"Log: message de saida: {request_body['message']}")
+        print(f"Log: codigo de execução: {request_body['execution_code']}")
+
+else:
+    print(f"Log: arquivo gerado: {request_body['filename']}")
+    print("Log: Gravação concluida com sucesso")
+    print(f"Log: message de saida: \n{request_body['message']}")
+print("Log: processo concluido.")
--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def flash_board(file_path, board, busdev):
 @app.route("/flash_9k", methods=["POST"])
 def flash_route_9k():
     board = "tangnano9k"
-    busdev = "01:001"
+    busdev = "01:046"
 
     if not "file" in request.files:
         return jsonify({"success": False, "error": "Arquivo não enviado"}), 400
@@ -75,13 +75,16 @@ def flash_route_9k():
 
     r, content = flash_board(file_path, board, busdev)
 
-    return jsonify({"success": True, "filename": filename, "message": content, "execution_code": r}), 201
+    if r != 0:
+        return jsonify({"success": False, "filename": filename, "message": content, "execution_code": r}), 400
+    else:
+        return jsonify({"success": False, "filename": filename, "message": content, "execution_code": r}), 201
 
 
 @app.route("/flash_20k", methods=["POST"])
 def flash_route_20k():
     board = "tangnano20k"
-    busdev = "01:008"
+    busdev = "01:045"
 
     if not "file" in request.files:
         return jsonify({"success": False, "error": "Arquivo não enviado"}), 400
@@ -106,7 +109,10 @@ def flash_route_20k():
 
     r, content = flash_board(file_path, board, busdev)
 
-    return jsonify({"success": True, "filename": filename, "message": content, "execution_code": r}), 201
+    if r != 0:
+        return jsonify({"success": False, "filename": filename, "message": content, "execution_code": r}), 400
+    else:
+        return jsonify({"success": False, "filename": filename, "message": content, "execution_code": r}), 201
 
 
 if __name__ == "__main__":
